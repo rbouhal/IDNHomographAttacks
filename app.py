fcde9d7edde60083a9e197cb2decaccb9f9cad45
Andrew Merdes <merdea03@vt.edu>
2024-04-21 13:56:37 -0400

    added ID3 algorithm implementation

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 import math
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -130,6 +131,271 @@ def predict_domain_validity(domain, domain_data):
     
     #return 'valid' if prediction is 1, 'invalid' otherwise
     return 'valid' if prediction[0] == 1 else 'invalid', accuracy
+#this function trains the ID3 decision tree with the current csv data
+def trainID3():
+    df1 = pd.read_csv('static/valid-domains.csv')
+    df2 = pd.read_csv('static/invalid-domains.csv')
+    #format the data
+    valNP = df1.to_numpy()
+    invNP = df2.to_numpy()
+    valNP = np.delete(valNP, 0, axis=1)
+    valNP = np.delete(valNP, -1, axis=1)
+    invNP = np.delete(invNP, 0, axis=1)
+    invNP = np.delete(invNP, -1, axis=1)
+    val = dataAnalysis(valNP)
+    inv = dataAnalysis(invNP)
+    valCount = 0
+    for ind in range(len(val)):
+        valCount = valCount + 1
+        val[ind][33] = 1
+    fullData = np.concatenate((val, inv), axis=0)
+
+    ctLst = []
+    for ele in fullData[0]:
+        ctLst.append(2)
+    totalCount = 0
+    for ele in fullData:
+        totalCount = totalCount + 1
+    tree = []
+    level = 0
+    #train the model using formated data
+    return ID3(fullData, ctLst, valCount, totalCount - valCount, tree, level)
+#predicts a domain using the ID3 algorithm given a csv data array
+#if the funciton returns 1, domain is valid, if it returns 0 it is invalid
+def predict_ID3(csvInput):
+  tree = trainID3()
+  input = np.array(csvInput)
+  conIn = inputConversion(input)
+  return ID3Classify(conIn, tree)
+
+
+#this function converts some of the data features to a binary format
+def dataAnalysis(data):
+  numData = np.delete(data, -1, axis=1)
+  binData = np.zeros((len(numData), 34))
+  for ind, ele in enumerate(numData):
+    #feature 1 extrapolation
+    fin = 0
+    for i in range(15):
+      if i == ele[0]:
+        binData[ind, i] = 1
+        fin = 1
+    if fin == 0:
+      binData[ind, 15] = 1
+    #feature 2 extrapolation
+    fin = 0
+    for i in range(8):
+      if i == ele[1]:
+        binData[ind, 16 + i] = 1
+        fin = 1
+    if fin == 0:
+      binData[ind, 24] = 1
+    #feature 3 extrapolation
+    fin = 0
+    for i in range(4):
+      if i == ele[2]:
+        binData[ind, 25 + i] = 1
+        fin = 1
+    if fin == 0:
+      binData[ind][29] = 1
+    #feature 6 extrapolation
+    fin = 0
+    for i in range(2):
+      if i == ele[5]:
+        binData[ind, 30 + i] = 1
+        fin = 1
+    if fin == 0:
+      binData[ind, 32] = 1
+  return binData
+#this function implements the ID3 algorithm for a decision tree
+def ID3(data, catNumOptionsLst, c1, c0, tree, level):
+  infoGainLst = []
+  classLabel = np.array(data[:, -1])
+  if c1 == 0:
+    tree.append([-1, level])
+    return
+  if c0 == 0:
+    tree.append([-2, level])
+    return
+  if len(catNumOptionsLst) == 0:
+    c0Count = 0
+    c1Count = 0
+    for ele in data:
+      if ele[-1] == 0:
+        c0Count = c0Count + 1
+      else:
+        c1Count = c1Count + 1
+    if c0Count < c1Count:
+      tree.append([-2, level])
+    else:
+      tree.append([-1, level])
+    return
+  for ind,op in enumerate(catNumOptionsLst):
+    currCat = np.array(data[:, ind])
+    catLabel = np.column_stack((currCat, classLabel)) #combine the category data and label data
+    sortInd = np.argsort(catLabel[:,0]) #sort categores for spliting
+    sCat = catLabel[sortInd]
+    currInfoG = 0
+    overallEntropy = entropy(c0, c1)
+    #now calculate information gain of current feature
+    for i in range(op):
+      mask = (sCat[:, 0] == i)
+      catI = sCat[mask]
+      if catI.size != 0:
+        splitClass1 = 0
+        if(np.size(catI) == 2):
+          splitClass1 = catI[0][1]
+        else:
+          splitClass1 = catI.sum(axis=1)[1]
+        currInfoG += np.size(catI)/np.size(sCat) * entropy(splitClass1, np.size(catI) - splitClass1)
+      else:
+        currInfoG = 999999
+        break
+    currInfoG = overallEntropy - currInfoG
+    infoGainLst.append([currInfoG, ind])
+  #determine the highest information gain
+  maxV = max([sublist[0] for sublist in infoGainLst])
+  if maxV == 0:
+    print("Limit reached")
+    c0Count = 0
+    c1Count = 0
+    for ele in data:
+      if ele[-1] == 0:
+        c0Count = c0Count + 1
+      else:
+        c1Count = c1Count + 1
+    if c0Count < c1Count:
+      tree.append([-2, level])
+    else:
+      tree.append([-1, level])
+    return
+  maxInd = -1
+  for ele in infoGainLst:
+    if maxV in ele:
+      maxInd = ele[1]
+  currCat = np.array(data)
+
+  sortInd = np.argsort(currCat[:,maxInd]) #sort categores for spliting
+  sCat = currCat[sortInd]
+  zeroSplt = []
+  oneSplt = []
+  #now split the two max info lists
+  for ln in sCat:
+    if ln[maxInd] == 1:
+      oneSplt.append(ln.tolist())
+    else:
+      zeroSplt.append(ln.tolist())
+  sp0 = np.array(zeroSplt)
+  sp1 = np.array(oneSplt)
+  tree.append([maxInd, level])
+  sp0C0 = 0
+  sp0C1 = 0
+  sp1C0 = 0
+  sp1C1 = 0
+  for ele in sp0[:, -1]:
+    if ele == 1:
+      sp0C1 = sp0C1 + 1
+    else:
+      sp0C0 = sp0C0 + 1
+  for ele in sp1[:, -1]:
+    if ele == 1:
+      sp1C1 = sp1C1 + 1
+    else:
+      sp1C0 = sp1C0 + 1
+  catNumOptionsLst.pop(maxInd)
+  sp0 = np.delete(sp0, maxInd, axis=1)
+  sp1 = np.delete(sp1, maxInd, axis=1)
+  #recursively call the function on the first split
+  ID3(sp0,catNumOptionsLst.copy(), sp0C1, sp0C0, tree, level + 1)
+  #recursively call the function on the second split
+  ID3(sp1,catNumOptionsLst.copy(), sp1C1, sp1C0, tree, level + 1)
+
+  return tree
+#helper function for calculating entropy
+def entropy(class0, class1):
+  if class0  == 0:
+    return 0
+  if class1 == 0:
+    return 0
+  p0 = class0/ (class0 + class1)
+  p1 = class1 / (class0 + class1)
+  ent = -(p0 * math.log(p0, 2) + p1 * math.log(p1, 2))
+  return ent
+#helper funciton for calculating information gain
+def informationGain(origEntro, split1, split2, class0s1, class0s2):
+  info = origEntro - (split1/(split1 + split2) * entropy(class0s1, (split1 - class0s1)) + split2/(split1 + split2) * entropy(class0s2, (split2 - class0s2)))
+  return info
+#This module takes a given ID3 tree and determine the value of a single input
+def ID3Classify(input, tree):
+  skip = -1
+  lvl = 0
+  skCount = 0
+  for node in tree:
+    lvl = node[1]
+    catNum = node[0]
+
+    if skip != -1:
+      if lvl == skip:
+        skCount = skCount + 1
+      if skCount == 2:
+        skCount = 0
+        skip = -1
+        if catNum == -1:
+          return 0
+        if catNum == -2:
+          return 1
+        catVal = input[catNum]
+        if catVal == 1:
+          skip = lvl + 1
+        else:
+          input = np.delete(input, catNum)
+    else:
+      if catNum == -1:
+        return 0
+      if catNum == -2:
+        return 1
+      catVal = input[catNum]
+      if catVal == 1:
+        skip = lvl + 1
+      else:
+        input = np.delete(input, catNum)
+  return -1
+#converts input data into correct format for ID3 algorithm
+def inputConversion(input):
+  binInput = np.zeros(34)
+  #feature 1 extrapolation
+  fin = 0
+  for i in range(15):
+    if i == input[0]:
+      binInput[i] = 1
+      fin = 1
+  if fin == 0:
+    binInput[15] = 1
+  #feature 2 extrapolation
+  fin = 0
+  for i in range(8):
+    if i == input[1]:
+      binInput[16 + i] = 1
+      fin = 1
+  if fin == 0:
+    binInput[24] = 1
+  #feature 3 extrapolation
+  fin = 0
+  for i in range(4):
+    if i == input[2]:
+      binInput[25 + i] = 1
+      fin = 1
+  if fin == 0:
+    binInput[29] = 1
+  #feature 6 extrapolation
+  fin = 0
+  for i in range(2):
+    if i == input[5]:
+      binInput[30 + i] = 1
+      fin = 1
+  if fin == 0:
+    binInput[32] = 1
+  return binInput
 
 if __name__ == '__main__':
     app.run(debug=True)
